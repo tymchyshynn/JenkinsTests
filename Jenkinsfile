0f5229392d34dfb48114edbbd29db91e5445c33f
tymchyshynn <tymchyshynn@gmail.com>
2019-01-17 23:12:01 +0200

    test1

--- Jenkinsfile
+++ Jenkinsfile
@@ -29,7 +29,7 @@ node('master') {
     }
     stage('Restore NuGet') 
 	{ 
-       bat '"C:\\Dev\\nuget.exe" restore PhpTravels.UITests.sln'
+       powershell "build.ps1 RestoreNuGetPackages"
     }
 	stage('Build Solution')
     {
@@ -71,8 +71,7 @@ catchError
 
         archiveArtifacts '*.xml'
         nunit testResultsPattern: 'TestResult1.xml, TestResult2.xml'
-
-        
+  
     }
 }
 	
---
+++ build.ps1
@@ -0,0 +1,86 @@
+#Requires -Version 5.0
+
+param
+(
+    [Parameter()]
+    [String[]] $TaskList = @("RestorePackages", "Build", "CopyArtifacts"),
+    
+    # Also add following parameters: 
+    #   Configuration
+    #   Platform
+    #   OutputPath
+    # And use these parameters inside BuildSolution function while calling for MSBuild.
+    # Use /p swith to pass the parameter. For example:
+    #   MSBuild.exe src/Solution.sln /p:Configuration=$Configuration
+    # More info here: https://docs.microsoft.com/en-us/visualstudio/msbuild/common-msbuild-project-properties?view=vs-2017
+
+    [Parameter()]
+    [String] $BuildArtifactsFolder
+)
+
+$NugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$NugetExe = Join-Path $PSScriptRoot "nuget.exe"
+# Define additional variables here (MSBuild path, etc.)
+
+Function DownloadNuGet()
+{
+    if (-Not (Test-Path $NugetExe)) 
+    {
+        Write-Output "Installing NuGet from $NugetUrl..."
+        Invoke-WebRequest $NugetUrl -OutFile $NugetExe -ErrorAction Stop
+    }
+}
+
+Function RestoreNuGetPackages()
+{
+    DownloadNuGet
+    Write-Output 'Restoring NuGet packages...'
+	 bat '"C:\\Dev\\nuget.exe" restore PhpTravels.UITests.sln'
+    # NuGet.exe call here
+}
+
+Function BuildSolution()
+{
+    Write-Output "Building '$Solution' solution..."
+    # MSBuild.exe call here
+}
+
+Function CopyBuildArtifacts()
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String] $SourceFolder,
+        [Parameter(Mandatory)]
+        [String] $DestinationFolder
+    )
+
+    # Copy all files from $SourceFolder to $DestinationFolder
+    #
+    # Useful commands:
+    #   Test-Path - check if path exists
+    #   Remove-Item - remove folders/files
+    #   New-Item - create folder/file
+    #   Get-ChildItem - gets items from specified path
+    #   Copy-Item - copies item into destination folder
+    #
+    #     NOTE: you can chain methods using pipe (|) symbol. For example:
+    #           Get-ChildItem ... | Copy-Item ...
+    #
+    #           which will get items (Get-ChildItem) and will copy them (Copy-Item) to the target folder
+}
+
+foreach ($Task in $TaskList) {
+    if ($Task.ToLower() -eq 'restorepackages')
+    {
+        RestoreNuGetPackages
+    }
+    if ($Task.ToLower() -eq 'build')
+    {
+        BuildSolution
+    }
+    if ($Task.ToLower() -eq 'copyartifacts')
+    {
+        CopyBuildArtifacts "YOUR FOLDER WITH BUILT DLLS" "$BuildArtifactsFolder"
+    }
+}
